50aaf3271c2eebbc77f3f70fc53908ed8a677a8f
Philip Carlsson <philipcarlsson@hotmail.com>
2022-02-23 14:22:02 +0100

    setting up css for Footer.js

--- dynamic-react-app/src/components/Footer.js
+++ dynamic-react-app/src/components/Footer.js
@@ -1,8 +1,9 @@
-import React from 'react'
+import React from 'react';
+import styles from "../cssmodules/footer.module.css";
 
 function Footer() {
   return (
-    <div>Footer</div>
+    <div className={styles.footer}>Footer</div>
   )
 }
 

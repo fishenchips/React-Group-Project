bd7bf945d24e60965aacc959a089b9f4494b3097
Moises771 <Julianbrb23@gmail.com>
2022-02-24 10:42:24 +0100

    Added Philip's Code
    Added Philip's part to my doc.

--- dynamic-react-app/src/App.js
+++ dynamic-react-app/src/App.js
@@ -1,11 +1,25 @@
+import logo from './logo.svg';
 import './App.css';
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from "react-router-dom"
+import Footer from './components/Footer';
+import Product from './Pages/Product';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-     hello
-      </header>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/:id" element={ <Product /> } ></Route>
+
+        </Routes>
+
+
+        <Footer />
+      </BrowserRouter>
     </div>
   );
 }
---
+++ dynamic-react-app/src/components/Footer.js
@@ -0,0 +1,19 @@
+//Phillips code copied
+
+import React from 'react';
+import styles from "../cssmodules/footer.module.css";
+import Clock from './clock';
+
+
+function Footer() {
+  return (
+    <div className={styles.footer}>
+        <div>Shopping info</div> 
+        <div>Contact us</div>
+        <div>Payment Methods</div>
+        <div><Clock /></div>
+    </div>
+  )
+}
+
+export default Footer

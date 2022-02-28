d121041019d2ecc60ca7c9ef71534a23f189f323
Philip Carlsson <philipcarlsson@hotmail.com>
2022-02-28 11:44:23 +0100

    creating some animations

---
+++ dynamic-react-app/src/animations.js
@@ -0,0 +1,11 @@
+export const headerSlide = {
+    start: {
+        x: "-35vw",
+        scale:0.1
+    },
+    end: {
+        x: 0,
+        scale: 1,
+        transition : {duration: 2}
+    }  
+}
--- dynamic-react-app/src/pages/Product.js
+++ dynamic-react-app/src/pages/Product.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import { useParams } from "react-router-dom"
 import styles from "../cssmodules/product.module.css"
+import {motion} from "framer-motion";
 
 function Product() {
     const params = useParams();
@@ -24,11 +25,40 @@ function Product() {
 
   return (
     <article className={styles.productPage}>
-        <h2 className={styles.productTitle}>{product.title}</h2>
+        <motion.h2  
+            className={styles.productTitle}
+            initial={{
+                y: "-10vh",
+                scale: 0.1
+            }}
+            animate={{
+                y: 0,
+                scale: 1.7
+            }}
+            transition= {{
+                duration: 2
+            }}
+        >
+            {product.title}
+        </motion.h2>
         <div className={styles.productWrapper}>
-            <div className={styles.imgDiv}>
+            <motion.div 
+                className={styles.imgDiv}
+                initial={{
+                    x: "-35vw",
+                    scale:0.1
+                }}
+                animate={{
+                    x: 0,
+                    scale: 1
+                }}
+                transition= {{
+                    duration: 2
+                }}
+
+            >
                 <img className={styles.img} src={product.url}/>
-            </div>
+            </motion.div>
             <div className={styles.productAside}>
                 <h3>{product.price} SEK</h3>
                 <button className={styles.cartBtn}>add to cart</button>

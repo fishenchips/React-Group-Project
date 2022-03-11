5d84d7a59f0d740418b98f7e5e0346773017457a
Philip Carlsson <philipcarlsson@hotmail.com>
2022-03-11 14:33:39 +0100

    changing an a tag to Link

--- dynamic-react-app/src/components/Product.js
+++ dynamic-react-app/src/components/Product.js
@@ -1,11 +1,13 @@
 import React from 'react';
+import { Link } from "react-router-dom"
+
 
 export default function Product(props) {
   const { product, onAdd } = props;
   return (
     <div>
       <section  key={product.id}>
-                      <a href={`/products/+ ${product.id}`}> <img src={product.url} alt={product.description} width='30%' height='30%'></img></a>
+                      <Link to={`/products/+ ${product.id}`}> <img src={product.url} alt={product.description} width='30%' height='30%'></img></Link>
                         <h1 className='header1'>{product.title}</h1> 
                         <h3 className='header3'>{product.price}SEK</h3>                        
                         <p>{product.description}</p>
